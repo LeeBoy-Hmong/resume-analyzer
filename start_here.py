d9b0ff02fc9e1c1191be0fec4db1d9bca18b3680
LeeBoy-Hmong <michael1998lee@outlook.com>
2025-03-19 14:34:10 -0500

    initial commit

---
+++ start_here.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+# Install the Chrome Webdriver from: https://sites.google.com/chromium.org/driver/
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
+
+time.sleep(10)
+
+driver.quit()
+
+
